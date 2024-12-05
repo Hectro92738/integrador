f49b232f526c1276319256ca1726215681652856
kerroris <kerroris@gmail.com>
2024-12-05 01:15:41 -0600

    insert point e ingreso y ventas

--- routes/web.php
+++ routes/web.php
@@ -33,10 +33,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/detalle-venta/{id}', [VentasController::class, 'detalleVenta'])->name('detalleVenta');
     Route::get('/status-articulo/{id}', [VentasController::class, 'statusArticulo'])->name('statusArticulo');
     Route::post('/insert-venta', [VentasController::class, 'insertVenta'])->name('insertVenta');
+
+    // rutas para vender, y el inicio
+    Route::get('/inicio', [ViewController::class, 'inicio'])->name('inicio');
+    Route::get('/vender', [ViewController::class, 'vender'])->name('vender');
+    Route::get('/ventas', [ViewController::class, 'ventas'])->name('ventas');
+
+
+    
 });
 
 // .......... ADMIN ..........
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/gestion-productos', [ViewController::class, 'gestionProductos'])->name('gestionProductos');
+
+    Route::get('/view-ingresos', [ViewController::class, 'viewingresos'])->name('ingresos.view');
     
 });
